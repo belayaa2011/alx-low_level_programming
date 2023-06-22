cf6533f2f27d3b218702dd149c1fbc7473bcc727
belay <belayaa2011@gmail.com>
2023-06-21 21:45:01 -0700

    all

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	/* trailing whitespace */
+	printf("Infinite loop incoming :(\n");
+	/**
+	* i = 0;
+	*
+	* while (i < 10)
+	* {
+	* putchar(i);
+	* }
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+	/*trailing whitespace */
+	return (0);
+}
