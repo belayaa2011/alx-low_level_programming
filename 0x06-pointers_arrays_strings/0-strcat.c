8c581adf2f207c7918ead0d4390b5db5b3a67d63
belay <belayaa2011@gmail.com>
2023-06-28 22:45:17 -0700

    all

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenate two strings
+ * @dest: a string to be concatenate
+ * @src: a string to be concatenate
+ * Return: know return is dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n, m;
+
+	n = 0;
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	m = 0;
+	while (src[m] != '\0')
+	{
+		dest[n] = src[m];
+		m++;
+		n++;
+	}
+	dest[n] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - a function that replaces a letter with 13th letter
+ * @s: the array os string to be replaced
+ * Return: return the string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char norm[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == norm[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
